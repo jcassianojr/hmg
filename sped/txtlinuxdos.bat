6d73f2ca1be6359ba3146e18d567274adf32542e
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2021-03-17 19:43:00 -0300

    ajuste ms

--- sped/txtlinuxdos.bat
+++ sped/txtlinuxdos.bat
@@ -1,3 +1,36 @@
 attrib -r /s
-unix2dos *.txt
-unix2dos *.data
+rem unix2dos *.txt
+rem unix2dos *.data
+unix2dos contrbcnpj.txt
+unix2dos contodos.txt
+unix2dos connormal.txt
+unix2dos conoutros.txt
+unix2dos contrib_todas_sit.txt
+unix2dos cci_resumido.txt
+unix2dos cpa.txt
+unix2dos cad_resumido_ativos.txt
+unix2dos cad_resumido_total_cnpj.txt
+unix2dos ics_ativo.txt
+unix2dos ics_baixado.txt
+unix2dos cadativo.txt
+unix2dos arq_pessoa_fisica.txt
+unix2dos cad_resumido_total.txt
+unix2dos cad_resumido_total_cnpj.txt
+unix2dos lista_cadastro1.txt
+unix2dos rjcontrib.txt
+unix2dos cadastro_de_contribuinte_simplificado.txt
+unix2dos ativos_icms.txt
+unix2dos lista_cadastro_restricao1.txt
+unix2dos contribuinte.txt
+unix2dos cadastro.txt
+unix2dos cadastro_cnpj.txt
+unix2dos sintegra.txt
+unix2dos ativos.txt
+unix2dos cancelados.txt
+unix2dos contrbpjativo.txt
+unix2dos contrbespecbaixa.txt
+unix2dos contrbpfativo.txt
+unix2dos contrbpfinativo.txt
+unix2dos contrbpfoutro.txt
+unix2dos contrbpjinativo.txt
+unix2dos contrbpjoutro.txt
